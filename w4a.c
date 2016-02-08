80a659320f8bc7e0d4b2a5bad630c5f27c2c2651
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-02-08 13:56:52 -0500

    added w4a.c

---
+++ w4a.c
@@ -0,0 +1,18 @@
+/*
+This program is a math tutoring program
+it will give the user a choice of mathematical functions
+it will then ask for input numbers
+and show the result to the user 
+(depending on the function chosen, and input numbers)
+TODO: improve the description
+*/
+
+#include <stdio.h>
+
+int main(void){
+	char choice = 0;
+	printf("Welcome to Math Calculator!!!\n");
+	while( )
+
+}
+
