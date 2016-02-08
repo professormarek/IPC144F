4ae4102e7736cd42140cf23fcaa1a382b105cae6
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-02-08 14:44:51 -0500

    added is_prime function

--- w4a.c
+++ w4a.c
@@ -9,8 +9,25 @@ TODO: improve the description
 
 #include <stdio.h>
 
+/*
+this function accepts an integer argument and returns an integer
+that will be 0 if the number is not prime, and 1 if the number is prime
+*/
+int is_prime(int number){
+	int result = 1; //assume that number is prime to begin with
+	int i; // i is the loop control variable
+	for(i = 2; i < number; i++){
+		if(number % i == 0){
+			result = 0; // that means that number is not prime
+		}
+	}
+	return result;
+}
+
 int main(void){
 	char choice = 0;
+	int user_int = 0;
+	int result_int = 0;
 	printf("Welcome to Math Calculator!!!\n");
 	while(choice != 'q' ){
 		//present the menu
@@ -20,8 +37,15 @@ int main(void){
 		//an if statement, that looks at the current choice, and does something different, depending
 		if(choice == 'q' ){
 			printf("Thakns for using Math Claculator! Goodbye!\n");
+		}else if(choice == 'p' ){
+			//get an integer from the user
+			printf("Enter a positive integer:");
+			scanf("%d", &user_int);
+			result_int = is_prime(user_int);
+			//if statement to print out an appropriate message
 		}
 	}
+	return 0;//0 indicates the program terminated correctly
 
 }
 
