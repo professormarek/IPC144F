a66e17b0d746486eecc610400d665976e35fe97d
Marek Laskowski <marek.laskowski@matrix.senecac.on.ca>
2016-03-14 14:21:52 -0400

    added w7a.c

---
+++ w7a.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+#define MAX_BOOKS 10000
+
+/*
+this function clears the buffer
+*/
+void clearBuffer(void){
+	//variable to store the next character in the buffer
+	char c=0;
+	while(c != '\n'){
+		c = getchar();
+	}
+}
+
+/* this function accepts two integer arguments and validates user inpute to 
+ensure that the user enters a number between the two arguments
+once the input is validated, this function returns the users's choice as an 
+integer
+the arugments are listed between the ( ). They can be thought of as
+variables that are only available inside the { }
+when the fucntion is called, these arguments take on the values passed from the 
+calling function (ex. from main() )
+*/
+int getUserInteger(int minimum, int maximum){
+	//choose an invalid initial value for the user input
+	int userInput = minimum - 1;
+	/*the || operator means "OR" - it can be used to combine simple 
+	conditions into more complicated conditions */
+	while(userInput < minimum || userInput > maximum){
+		//ask the user to enter a number within the range
+		printf("Enter an integer between %d and %d: ", minimum, maximum);
+		//validate the input
+		scanf("%d", &userInput);
+		clearBuffer();		
+	}
+	return userInput;
+}
+
+/*
+this function does the same thing as getUserInteger but with the double type
+*/
+double getUserDouble(double minimum, double maximum){
+        //choose an invalid initial value for the user input
+        double userInput = minimum - 1;
+        /*the || operator means "OR" - it can be used to combine simple
+        conditions into more complicated conditions */
+        while(userInput < minimum || userInput > maximum){
+                //ask the user to enter a number within the range
+                printf("Enter an integer between %lf and %lf: ", minimum, maximum);
+                //validate the input
+                scanf("%lf", &userInput);
+                clearBuffer();
+        }
+        return userInput;
+}
+
+
+
+
+
+/*
+this program gets a bookstore inventory from the user
+and stores it in "parallel" arrays - meaning that
+we have multiple arrays, and each array holds a different
+piece of information about the book at that index
+after entering the books, it allows the user to search for books
+and retrieve their price
+*/
+int main(void){
+	//declare parallel arrays to store book information
+	//one array to store isbn
+	int isbn[MAX_BOOKS];
+	//one array to store prices
+	double price[MAX_BOOKS];
+	//one array to store quantities
+	int quantity[MAX_BOOKS];
+
+}
